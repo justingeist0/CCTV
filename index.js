61f4e418adc7d3bbb74be8267774dae27b59b921
Justin Geist <justingeist0@gmail.com>
2023-11-08 01:10:38 -0500

    organizing

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 const { run, getClients, storeClient, getDevices, storeDevice, setDeviceImages, getDevice } = require('./db')
+
 var admin = require("firebase-admin");
 var serviceAccount = require("./firebase-admin.json");
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
+
 const admins = ["iimezzooo@gmail.com", "justingeist0@gmail.com", "rylensalvi@gmail.com"]
 
 run().catch(console.dir);
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const fs = require('fs');
 const app = express();
 const version = 1
 
+app.set('view engine', 'ejs');
+
+app.use(express.static('public', { extensions: ['html'] }));
 app.use(bodyParser.json());
 
 const port = 3000;
@@ -43,34 +47,6 @@ const authenticateUser = (req, res, next) => {
     });
 };
 
-app.get('/', (req, res) => {
-  // Read the content of a file (e.g., "index.html")
-  fs.readFile('index.html', 'utf8', (err, data) => {
-    if (err) {
-      // Handle any errors
-      console.error(err);
-      res.status(500).send('Error reading the file.');
-    } else {
-      // Send the file content as the response
-      res.send(data);
-    }
-  });
-});
-
-app.get('/obs', (req, res) => {
-  // Read the content of a file (e.g., "index.html")
-  fs.readFile('obs.html', 'utf8', (err, data) => {
-    if (err) {
-      // Handle any errors
-      console.error(err);
-      res.status(500).send('Error reading the file.');
-    } else {
-      // Send the file content as the response
-      res.send(data);
-    }
-  });
-});
-
 app.get('/device/:id', async (req, res) => {
   const id = req.params.id;
   const data = await getDevice(id)
@@ -81,39 +57,11 @@ app.get('/get-version', async (req, res) => {
   res.json({version});
 });
 
-app.get('/add-client', authenticateUser, (req, res) => {
-  // Read the content of a file (e.g., "index.html")
-  fs.readFile('add-client.html', 'utf8', (err, data) => {
-    if (err) {
-      // Handle any errors
-      console.error(err);
-      res.status(500).send('Error reading the file.');
-    } else {
-      // Send the file content as the response
-      res.send(data);
-    }
-  });
-});
-
 app.post('/add-client', authenticateUser, async (req, res) => {
   const clientData = req.body;
   const store = await storeClient(clientData)
   console.log(store)
   res.json(store);
-});
-
-app.get('/add-device', authenticateUser, (req, res) => {
-  // Read the content of a file (e.g., "index.html")
-  fs.readFile('add-device.html', 'utf8', (err, data) => {
-    if (err) {
-      // Handle any errors
-      console.error(err);
-      res.status(500).send('Error reading the file.');
-    } else {
-      // Send the file content as the response
-      res.send(data);
-    }
-  });
 });
 
 app.post('/add-device', authenticateUser, async (req, res) => {
